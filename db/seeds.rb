1dfae9dd7c025ff28e003cc6c06fc809215abfd7
Sam Lau <samlau95@gmail.com>
2014-03-03 14:57:47 -0800

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,10 @@ def make_quizzes
   q = Quiz.create! lesson: 1,
                    version: 1
   q1 = q.questions.create! number: 1,
+                           points: 10,
                            content: 'What is 1 + 1?'
   q1.create_solution content: '2'
 end
 
 make_users
+make_quizzes
