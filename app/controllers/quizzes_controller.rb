dc170832e81e522bc80bc5ff1a043ef0d8838756
Sam Lau <samlau95@gmail.com>
2014-03-03 14:57:23 -0800

    Quiz controller actions

---
+++ app/controllers/quizzes_controller.rb
@@ -0,0 +1,49 @@
+class QuizzesController < ApplicationController
+
+  def take
+    @quiz = Quiz.find params[:id]
+    @questions = @quiz.questions.order :number
+    @questions.each do |q|
+      q.submissions.build student_id: current_user, quiz_id: @quiz
+    end
+  end
+
+  def submit
+    
+  end
+
+  def index
+    @quizzes = Quiz.all
+  end
+
+  def new
+    @quiz = Quiz.new
+  end
+
+  def create
+    @quiz = Quiz.new quiz_params
+    if @quiz.save
+      redirect_to quizzes_path, notice: 'Created quiz.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @quiz = Quiz.find params[:id]
+  end
+
+  def update
+    @quiz = Quiz.find params[:id]
+    if @quiz.update_attributes quiz_params
+      redirect_to quizzes_path, notice: 'Updated quiz.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Quiz.find(params[:id]).destroy
+    redirect_to quizzes_path, notice: 'Deleted quiz.'
+  end
+end
