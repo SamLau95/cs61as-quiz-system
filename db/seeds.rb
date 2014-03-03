7321f4b075570b215b07f6d4725c3a796d720b7c
Sam Lau <samlau95@gmail.com>
2014-03-03 15:20:12 -0800

    More questions for quiz 1 in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,17 @@ def make_quizzes
   q = Quiz.create! lesson: 1,
                    version: 1
   q1 = q.questions.create! number: 1,
-                           points: 10,
+                           points: 2,
                            content: 'What is 1 + 1?'
   q1.create_solution content: '2'
+  q2 = q.questions.create! number: 2,
+                           points: 3,
+                           content: 'What is 1 * 1?'
+  q2.create_solution content: '1'
+  q3 = q.questions.create! number: 3,
+                           points: 5,
+                           content: 'What is 10 + 1?'
+  q3.create_solution content: '11'
 end
 
 make_users
