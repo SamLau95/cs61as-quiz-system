53b934db383a5ce77497da319f1007acb480b44b
Sam Lau <samlau95@gmail.com>
2014-03-02 20:18:28 -0800

    Initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cs61asQuizzes::Application.config.secret_key_base = '35a9516ca045a241fe249f1a7d86f0fc27fa0637431717c12a52e57cd0a4b1ad0b989b51b2a21cc48b31e802c61793f3f3c3288021706f2fece69fac7078e2c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cs61asQuizzes::Application.config.session_store :cookie_store, key: '_cs61as_quizzes_session'
