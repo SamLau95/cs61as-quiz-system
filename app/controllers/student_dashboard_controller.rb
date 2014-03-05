aed508846f26b525cda12463e858f11c89b00206
Sam Lau <samlau95@gmail.com>
2014-03-05 00:39:05 -0800

    Authorize student dashboard

--- app/controllers/student_dashboard_controller.rb
+++ app/controllers/student_dashboard_controller.rb
@@ -1,5 +1,11 @@
 class StudentDashboardController < ApplicationController
+  before_filter :check_authorization
+
   def index
-    
   end
+
+  private
+    def check_authorization
+      authorize! :take, Quiz
+    end
 end
