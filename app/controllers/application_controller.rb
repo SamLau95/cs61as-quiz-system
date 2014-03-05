3f0179a1109fc71ff2b2b5abab8757a4659fe950
Sam Lau <samlau95@gmail.com>
2014-03-05 00:35:54 -0800

    Add access denied message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = 'You aren\'t allowed to view that page!'
+    redirect_to root_url
+  end
+
   def after_sign_in_path_for(user)
     if user.staff?
       staff_dashboard_path
