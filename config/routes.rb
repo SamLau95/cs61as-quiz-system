99f9e270c77b9199ca6a4a75116e365445dab637
Sam Lau <samlau95@gmail.com>
2014-03-06 17:19:37 -0800

    No show route for quizzes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Cs61asQuizzes::Application.routes.draw do
     get '', to: 'student_dashboard#index', as: :student_dashboard
   end
 
-  resources :quizzes
+  resources :quizzes, except: :show
   scope '/quizzes' do
     get '/:id/take', to: 'quizzes#take', as: :take_quiz
     post '/:id/submit', to: 'quizzes#submit', as: :submit_quiz
