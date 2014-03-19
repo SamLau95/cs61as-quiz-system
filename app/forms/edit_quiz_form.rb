b249efad1bdd494788b1b99a6d4b25c782c2a34d
Sam Lau <samlau95@gmail.com>
2014-03-19 01:29:00 -0700

    Validations for quiz form

--- app/forms/edit_quiz_form.rb
+++ app/forms/edit_quiz_form.rb
@@ -1,5 +1,7 @@
 # Form object for editing quizzes
 class EditQuizForm < Reform::Form
+  model :quiz
+
   property :lesson
   property :version
   property :retake
@@ -7,15 +9,27 @@ class EditQuizForm < Reform::Form
   collection :questions do
     property :quiz_id
     property :content
+    property :number
+    property :points
 
-    validates :quiz_id, true
+    validates :quiz_id, presence: true, numericality: true
     validates :content, presence: true
+    validates :number, presence: true, numericality: true
+    validates :points, presence: true,
+                       numericality: { greater_than_or_equal_to: 1,
+                                       less_than_or_equal_to: 10 }
   end
 
-  def save(params)
-    return false unless validate(params)
-    params[:new_submissions_attributes].all? do |_, attrs|
-      Submission.create attrs
+  validates :lesson, presence: true, numericality: true
+  validates :version, presence: true
+  validates :retake, presence: true
+  validate :points_add_to_10
+
+  private
+
+  def points_add_to_10
+    unless questions.map { |q| q.points.to_i }.sum == 10
+      errors.add :lesson, 'Points must sum to 10'
     end
   end
 end
