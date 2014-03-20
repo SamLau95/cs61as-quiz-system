2471a236fa6391c5314c40329d600aa3cc8cdb3f
Sam Lau <samlau95@gmail.com>
2014-03-20 15:15:13 -0700

    Configure databsae cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,7 +28,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -40,4 +40,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Have access to FactoryGirl methods
+  config.include FactoryGirl::Syntax::Methods
 end
