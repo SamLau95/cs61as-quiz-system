d49bc1ecf5d7487d1d0ebd8db61f247f6919f9d7
Sam Lau <samlau95@gmail.com>
2014-03-21 15:32:43 -0700

    Controller for requests

---
+++ app/controllers/quiz_requests_controller.rb
@@ -0,0 +1,9 @@
+# Controller for approving/cancelling quiz requests
+class QuizRequestsController < ApplicationController
+  load_and_authorize_resource
+
+  def approve
+    QuizRequest.find(params[:id]).approve!
+    redirect_to staff_dashboard_path
+  end
+end
