99ffecc5d9f68339b47a5c4180ebd853e9cd742e
Sam Lau <samlau95@gmail.com>
2014-03-20 23:12:34 -0700

    Annotate quiz request

--- app/models/quiz_request.rb
+++ app/models/quiz_request.rb
@@ -2,13 +2,15 @@
 #
 # Table name: quiz_requests
 #
-#  id          :integer          not null, primary key
-#  student_id  :integer
-#  quiz_number :integer
-#  created_at  :datetime
-#  updated_at  :datetime
+#  id         :integer          not null, primary key
+#  student_id :integer
+#  lesson     :integer
+#  created_at :datetime
+#  updated_at :datetime
 #
 
+# A QuizRequest is created when a student requests a quiz and is destroyed
+# after a student starts taking a quiz.
 class QuizRequest < ActiveRecord::Base
   belongs_to :student
 end
