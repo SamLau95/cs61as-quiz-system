4ea602cfcc12a873481e5e7f81d6113f7cb67928
Sam Lau <samlau95@gmail.com>
2014-03-20 23:13:56 -0700

    Make quiz request available in view

--- app/controllers/student_dashboard_controller.rb
+++ app/controllers/student_dashboard_controller.rb
@@ -1,13 +1,13 @@
 # Controller for the student dashboard
 class StudentDashboardController < ApplicationController
-
   def index
     @lessons = Quiz.lessons
+    @quiz_request = current_user.quiz_request
   end
 
   private
 
   def check_authorization
-    authorize! :take, Quiz
+    authorize! :request, Quiz
   end
 end
