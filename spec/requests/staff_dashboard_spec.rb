903e2597b024d1ae61d65f9d938cb2b44b5a2869
Sam Lau <samlau95@gmail.com>
2014-03-21 15:31:46 -0700

    Specs approving request

--- spec/requests/staff_dashboard_spec.rb
+++ spec/requests/staff_dashboard_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe 'The staff dashboard' do
   let(:staff) { create :staff }
   subject { page }
+  before { sign_in staff }
 
   describe 'when there are quizzes' do
     let!(:quizzes) { 4.times.map { create :quiz } }
-    before { sign_in staff }
+    before { visit staff_dashboard_path }
 
     it 'displays all quizzes' do
       quizzes.each do |quiz|
@@ -16,13 +17,38 @@ describe 'The staff dashboard' do
   end
 
   describe 'when there are quiz requests' do
-    let!(:quiz_requests) { 4.times.map { create :quiz_request } }
-    before { sign_in staff }
+    let!(:requests) { 4.times.map { create :quiz_request } }
+    let!(:approved_request) { create :quiz_request, approved: true }
+    before { visit staff_dashboard_path }
 
-    it 'displays all quiz requests' do
-      quiz_requests.each do |request|
+    it 'displays all unapproved quiz requests' do
+      requests.each do |request|
         expect(page).to have_content request.to_s
       end
     end
+
+    it 'does not show approved requests' do
+      expect(page).not_to have_content approved_request.to_s
+    end
+
+    it 'displays confirm links' do
+      requests.each do |request|
+        expect(page).to have_link "Approve: #{request.student}"
+      end
+    end
+
+    it 'displays cancel links' do
+      expect(page).to have_link 'Cancel'
+    end
+  end
+
+  describe 'approving a quiz request' do
+    let!(:request) { create :quiz_request }
+    before do
+      visit staff_dashboard_path
+      click_link "Approve: #{request.student}"
+    end
+
+    it { should_not have_content "Approve: #{request.student}" }
   end
 end
