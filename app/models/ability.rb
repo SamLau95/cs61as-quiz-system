f80c8cc8e9b71db2d318fecfc793c584a42342f2
Sam Lau <samlau95@gmail.com>
2014-03-22 18:54:35 -0700

    Authorization for take

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,6 +11,7 @@ class Ability
       can :manage, :all
     elsif user.student?
       can :make_request, Quiz
+      can :take, Quiz if user.approved_request?
       can :view, :student_dashboard
     end
   end
