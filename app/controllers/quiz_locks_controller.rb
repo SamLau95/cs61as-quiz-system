8b65bbc9bcb2a619903fcba5cbb0e138d71f1951
Sam Lau <samlau95@gmail.com>
2014-04-17 17:47:59 -0700

    Fix authorization and respond to js

--- app/controllers/quiz_locks_controller.rb
+++ app/controllers/quiz_locks_controller.rb
@@ -1,9 +1,12 @@
 # Controller for QuizLocks
 class QuizLocksController < ApplicationController
-  authorize_resource class: false
+  load_and_authorize_resource
 
   def lock
     quiz_lock = QuizLock.find params[:id]
     quiz_lock.lock!
+    respond_to do |format|
+      format.js
+    end
   end
 end
