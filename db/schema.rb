87a372e295fdb9e01e463ded148fe40510b36cb6
Sam Lau <samlau95@gmail.com>
2014-04-19 03:04:02 -0700

    Add index to options question_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417231530) do
+ActiveRecord::Schema.define(version: 20140419095709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20140417231530) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "options", ["question_id"], name: "index_options_on_question_id", using: :btree
 
   create_table "questions", force: true do |t|
     t.text     "content"
