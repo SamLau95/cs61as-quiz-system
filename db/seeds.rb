a4ad8b86b4d4d8c9117b0e617a5e91eaa39159c1
Sam Lau <samlau95@gmail.com>
2014-04-19 02:54:02 -0700

    Remove making submissions from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,19 +23,12 @@ def make_quizzes
                            lesson: 1,
                            difficulty: 'Easy'
   q1.create_solution content: '2'
-  q.submissions.create content: '2',
-                       student_id: 1,
-                       question_id: 1
   q2 = q.questions.create! number: 2,
                            points: 3,
                            content: 'What is 1 * 3?',
                            type: 'CheckboxQuestion',
                            lesson: 1,
                            difficulty: 'Medium'
-  q.submissions.create content: '3',
-                       student_id: 1,
-                       question_id: 2
-  q2.create_solution content: '3'
   q2.options.create content: '2'
   q2.options.create content: '3'
   q3 = q.questions.create! number: 3,
@@ -47,9 +40,6 @@ def make_quizzes
   q3.create_solution content: '11'
   q3.options.create content: '1'
   q3.options.create content: '11'
-  q.submissions.create content: '11',
-                       student_id: 1,
-                       question_id: 3
   q = Quiz.create! lesson: 2,
                    version: 1
   q1 = q.questions.create! number: 1,
