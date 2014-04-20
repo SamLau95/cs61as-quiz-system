5b6eeafe764d69e7875d0ce00398da4f2ceb251b
Charles Xue <charlesx2013@gmail.com>
2014-04-19 17:11:52 -0700

    Dropped options

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419235711) do
+ActiveRecord::Schema.define(version: 20140420001103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
-
-  create_table "options", force: true do |t|
-    t.text     "content"
-    t.integer  "question_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "options", ["question_id"], name: "index_options_on_question_id", using: :btree
 
   create_table "questions", force: true do |t|
     t.text     "content"
