17ac880b5dd18d990d0fc565aa202b2b1572b24c
Charles Xue <charlesx2013@gmail.com>
2014-04-24 03:56:49 -0700

    Added grade

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: grades
+#
+#  id          :integer          not null, primary key
+#  question_id :string(255)
+#  integer     :string(255)
+#  student_id  :string(255)
+#  grade       :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#  comments    :text             default("No Comments")
+#
+
 class Grade < ActiveRecord::Base
 end
