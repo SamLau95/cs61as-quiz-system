76e832a39da2d8d66c67cef196fb2ae86535825c
Charles Xue <charlesx2013@gmail.com>
2014-04-30 12:40:54 -0700

    Set default point values

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140430183810) do
+ActiveRecord::Schema.define(version: 20140430193927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 20140430183810) do
     t.integer  "question_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "points"
+    t.integer  "points",      default: 0
     t.integer  "number"
   end
 
