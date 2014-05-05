68ce4f22f85b3ed5d52e2e90f587cda85fba777c
Charles Xue <charlesx2013@gmail.com>
2014-05-04 20:58:42 -0700

    Finished grade, do touch ups later

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -3,20 +3,37 @@ class GradesController < ApplicationController
   def new
     qid, sid = params[:qid], params[:sid]
     @question = Question.find qid
-    @grade = Grade.create(question_id: qid, student_id: sid)
+    @grade = Grade.where(question_id: qid, student_id: sid).first_or_create
     redirect_to edit_grade_path(@grade, lesson: @question.lesson)
   end
 
   def edit
     grade = Grade.find params[:id]
-    @submission = Submission.where question_id: grade.question_id,
-                                   student_id: grade.student_id
+    @submission = Submission.find_by question_id: grade.question_id,
+                                     student_id: grade.student_id
     @question = Question.find grade.question_id
     @grade_form = EditGradeForm.new grade
     @lesson = params[:lesson]
   end
 
   def update
-    1/0
+    grade = Grade.find params[:id]
+    @submission = Submission.find_by question_id: grade.question_id,
+                                     student_id: grade.student_id
+    @question = Question.find grade.question_id
+
+    @grade_form = EditGradeForm.new grade
+    quiz = params[:quiz_id]
+    if @grade_form.validate_and_save grade_params
+      redirect_to view_quiz_path(quiz, student_id: grade.student_id)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def grade_params
+    params.require(:grade).permit!
   end
 end
--- app/forms/edit_grade_form.rb
+++ app/forms/edit_grade_form.rb
@@ -9,4 +9,16 @@ class EditGradeForm < Reform::Form
   property :lesson
 
   validates_presence_of :grade, :comments
+  validate :check_points
+
+  def validate_and_save(grade_params)
+    return false unless validate(grade_params)
+    Grade.find(id).update_attributes(grade_params)
+  end
+
+  def check_points
+    unless (1..10).include? @fields.grade.to_i
+      errors.add :grade, 'Invalid grade'
+    end
+  end
 end
