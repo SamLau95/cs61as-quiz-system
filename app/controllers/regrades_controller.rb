5a7746fde355d8861b4dd5c1e2d45b74289a1996
Charles Xue <charlesx2013@gmail.com>
2014-05-06 21:14:25 -0700

    Students can request a regrade

--- app/controllers/regrades_controller.rb
+++ app/controllers/regrades_controller.rb
@@ -1,2 +1,21 @@
 class RegradesController < ApplicationController
+  def create
+  	quiz = Quiz.find params[:regrade][:quiz_id]
+  	regrade = Regrade.new regrade_params
+  	if regrade.save
+  		flash[:success] = "You've submitted a regrade request!"
+  	else
+  		flash[:error] = "You didn't fill out all the required fields!"
+  	end
+  	redirect_to view_quiz_path(quiz, student_id: params[:regrade][:student_id])
+  end
+
+  def destroy
+  end
+
+  private
+
+  def regrade_params
+  	params.require(:regrade).permit!
+  end
 end
