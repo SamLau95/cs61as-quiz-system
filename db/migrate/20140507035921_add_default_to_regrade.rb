62ca72d15c6bfd40d6ffcf2fe022d1bbc5c8d686
Charles Xue <charlesx2013@gmail.com>
2014-05-06 21:00:11 -0700

    Added default false value to regrades

---
+++ db/migrate/20140507035921_add_default_to_regrade.rb
@@ -0,0 +1,5 @@
+class AddDefaultToRegrade < ActiveRecord::Migration
+  def change
+  	change_column :regrades, :graded, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507001943) do
+ActiveRecord::Schema.define(version: 20140507035921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20140507001943) do
   create_table "regrades", force: true do |t|
     t.integer  "student_id"
     t.integer  "quiz_id"
-    t.boolean  "graded"
+    t.boolean  "graded",     default: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "questions"
