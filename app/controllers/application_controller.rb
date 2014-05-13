2b58558fb9d0ffcac793f5bafdba5de236e76df1
Sam Lau <samlau95@gmail.com>
2014-05-12 19:51:34 -0700

    Remove the very helpful puts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
-    puts 'Access denied!'
-    puts "#{after_sign_in_path_for current_user}"
     flash[:error] = 'You aren\'t allowed to view that page!'
     if current_user
       redirect_to after_sign_in_path_for(current_user)
