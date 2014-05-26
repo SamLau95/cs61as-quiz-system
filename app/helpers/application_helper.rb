61608d180f8058ed0f3b8af313b9a38a7e8f50c8
Sam Lau <samlau95@gmail.com>
2014-05-25 20:17:24 -0700

    Syntax highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,17 @@ module ApplicationHelper
                    fenced_code_blocks: true, superscript: true,
                    underline: true, highlight: true, quote: true }
     renderer_options = { hard_wrap: true, filter_html: true }
-    Redcarpet::Markdown.new(
+
+    highlight_syntax Redcarpet::Markdown.new(
       Redcarpet::Render::HTML.new(renderer_options), md_options
-    ).render(text).html_safe
+    ).render(text)
+  end
+
+  def highlight_syntax(html)
+    doc = Nokogiri::HTML.fragment html
+    doc.search('code[@class]').each do |code|
+      code.replace Albino.colorize(code.text.rstrip, code[:class])
+    end
+    doc.to_s.html_safe
   end
 end
