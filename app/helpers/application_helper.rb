5ffac7c2914a826f0168e66aafb881eb585ea992
Sam Lau <samlau95@gmail.com>
2014-05-25 19:25:39 -0700

    To markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # Other helpers
 module ApplicationHelper
-
+  def markdown(text)
+    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis,
+               :fenced_code, :gh_blockcode]
+    Redcarpet.new(text, *options).to_html.html_safe
+  end
 end
