fc22791330b8241d4c15fff1b5d7d88269d04786
Charles Xue <charlesx2013@gmail.com>
2014-05-27 17:28:25 -0700

    Stupid dog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  protected 
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
