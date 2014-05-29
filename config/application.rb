5db91d36a742adcab7d774416826ebdb28e60e72
Charles Xue <charlesx2013@gmail.com>
2014-05-28 21:42:32 -0700

    Moved csv

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'csv'
 require 'rails/all'
+require 'csv'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
