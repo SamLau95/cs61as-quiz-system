8b076c90bcad7296869051fde4e89614499de2b9
Charles Xue <charlesx2013@gmail.com>
2014-06-05 19:01:51 -0700

    hound stuff

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -31,7 +31,7 @@ class QuizzesController < ApplicationController
       @quiz_form = TakeQuizForm.new q
       inject_current_user_into! params
       if @quiz_form.validate_and_save params[:quiz]
-        TakenQuiz.create student_id: ql.student_id, 
+        TakenQuiz.create student_id: ql.student_id,
                          quiz_id: ql.quiz_id,
                          lesson: q.lesson
         ql.destroy
