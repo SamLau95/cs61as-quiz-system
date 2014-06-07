336f6c9b4793e6120ccd6072865d0567e7765a72
Charles Xue <charlesx2013@gmail.com>
2014-06-07 01:46:17 -0700

    Update student.rb

--- app/models/users/student.rb
+++ app/models/users/student.rb
@@ -107,7 +107,7 @@ class Student < User
   def self.get(search)
     return nil if search.blank? || Student.all.blank?
     search.downcase!
-    Student.all.select { |s| s.first_name.downcase.include?(search) || 
+    Student.all.select { |s| s.first_name.downcase.include?(search) ||
                              s.last_name.downcase.include?(search) ||
                              s.to_s.downcase.include?(search) ||
                              s.login.downcase.include?(search) }
