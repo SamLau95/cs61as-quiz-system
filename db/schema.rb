05a1eec361257121363c8933c82331c28175a3d8
Charles Xue <charlesx2013@gmail.com>
2014-07-18 02:35:39 -0700

    added info

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711073255) do
+ActiveRecord::Schema.define(version: 20140718075456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,11 +139,11 @@ ActiveRecord::Schema.define(version: 20140711073255) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: ""
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -154,6 +154,7 @@ ActiveRecord::Schema.define(version: 20140711073255) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "login",                  default: ""
+    t.boolean  "added_info",             default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
