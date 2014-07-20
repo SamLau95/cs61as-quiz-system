17067c0278b15a0142020b86a887d1e13979c368
Charles Xue <charlesx2013@gmail.com>
2014-07-19 21:17:41 -0700

    sort by login

--- app/models/users/user.rb
+++ app/models/users/user.rb
@@ -26,6 +26,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  default_scope { order('login') }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
