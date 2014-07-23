b5c9330a48d825336091aa80f8f4ccd542dc7a47
Sam Lau <samlau95@gmail.com>
2014-07-22 23:33:37 -0700

    Fix sign_in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -2,7 +2,7 @@ include ApplicationHelper
 
 def sign_in(user)
   visit root_path
-  fill_in 'Email', with: user.email
+  fill_in 'Login', with: user.login
   fill_in 'Password', with: user.password
   click_button 'Sign in'
 end
