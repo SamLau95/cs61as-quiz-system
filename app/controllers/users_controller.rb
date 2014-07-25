6eca7c0fa3c0d48c4db7c6095a634d73958122fe
Charles Xue <charlesx2013@gmail.com>
2014-07-25 02:26:48 -0700

    fixed bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,14 @@ class UsersController < ApplicationController
     if @edit_form.validate_and_save params[:user]
       user.update_with_password(user_params)
       user.update_attributes(user_params)
-      sign_in(user, bypass: true)
       if user.has_info?
         flash[:success] = 'Updated profile!'
         user.update_attribute(:added_info, true)
       end
+      sign_in(user, bypass: true)
       redirect_to after_sign_in_path_for(user)
     else
+      flash[:error] = "You're missing some fields!"
       render 'edit'
     end
   end
