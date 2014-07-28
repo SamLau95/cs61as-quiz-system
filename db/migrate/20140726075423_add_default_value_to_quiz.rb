759050410971325263a51c079425b67105d33d43
Charles Xue <charlesx2013@gmail.com>
2014-07-28 03:03:41 -0700

    more hound

--- db/migrate/20140726075423_add_default_value_to_quiz.rb
+++ db/migrate/20140726075423_add_default_value_to_quiz.rb
@@ -1,5 +1,5 @@
 class AddDefaultValueToQuiz < ActiveRecord::Migration
   def change
-    change_column :quizzes, :version, :integer, default: 0 
+    change_column :quizzes, :version, :integer, default: 0
   end
 end
