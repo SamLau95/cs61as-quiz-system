7d35cf37d015fe137c4a24675be4a63bb23bdfdd
Charles Xue <charlesx2013@gmail.com>
2014-08-07 00:33:02 -0700

    more tests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,14 +23,15 @@ Cs61asQuizzes::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :authentication => :plain,
-    :domain => 'gmail.com',
-    :user_name => 'username@gmail.com',
-    :password => 'secret'
-   }
+  config.action_mailer.smtp_settings = {
+     :address              => "smtp.gmail.com",
+     :port                 => 587,
+     :domain               => 'domain.com',
+     :user_name            => 'email_address@domain.com',
+     :password             => 'password',
+     :authentication       => :plain,
+     :enable_starttls_auto => true  
+  }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
