8ad3b4fb257b69a7b67735dd4046b7dec588ff22
Charles Xue <charlesx2013@gmail.com>
2014-08-07 00:18:59 -0700

    more hound

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -1,37 +1,37 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Quiz do
-  let!(:quiz1) { create :quiz_with_questions, lesson: '1' }
-  let!(:quiz2) { create :quiz, lesson: '2' }
-  let!(:request1) { create :quiz_request, lesson: '1', retake: false }
+  let!(:quiz1) { create :quiz_with_questions, lesson: "1" }
+  let!(:quiz2) { create :quiz, lesson: "2" }
+  let!(:request1) { create :quiz_request, lesson: "1", retake: false }
 
-  let!(:question1) { create :question, difficulty: 'Easy', lesson:  '2' }
-  let!(:question2) { create :question, difficulty: 'Medium', lesson: '2' }
-  let!(:question3) { create :question, difficulty: 'Hard', lesson: '2' }
+  let!(:question1) { create :question, difficulty: "Easy", lesson:  "2" }
+  let!(:question2) { create :question, difficulty: "Medium", lesson: "2" }
+  let!(:question3) { create :question, difficulty: "Hard", lesson: "2" }
 
-  describe '.lessons' do
-    it 'should return an array of published lessons' do
-      expect(Quiz.lessons).to eq(['1', '2'])
+  describe ".lessons" do
+    it "should return an array of published lessons" do
+      expect(Quiz.lessons).to eq(["1", "2"])
     end
   end
 
-  describe '.choose_one' do
+  describe ".choose_one" do
     let(:chosen_quiz) { Quiz.choose_one(request1) }
-    it 'should choose a random quiz' do
+    it "should choose a random quiz" do
       expect(chosen_quiz.lesson).to eq(request1.lesson)
       expect(chosen_quiz.retake).to eq(request1.retake) 
     end
   end
 
-  describe '.to_s' do
+  describe ".to_s" do
     it "should return the Quiz lesson and version" do
       expect(quiz1.to_s).to eq("Quiz 1a#{quiz1.version}")
       expect(quiz2.to_s).to eq("Quiz 2a#{quiz2.version}")
     end
   end
 
-  describe '#new_submissions' do
-    it 'should create new submissions' do
+  describe "#new_submissions" do
+    it "should create new submissions" do
       expect(quiz1.questions.length).to eq(3)
       expect(quiz1.new_submissions.length)
             .to eq(3)
@@ -41,9 +41,9 @@ describe Quiz do
     end
   end
 
-  describe '#next_number' do
-    it 'should return the question number for the quiz' do
-      pending('implement later')
+  describe "#next_number" do
+    it "should return the question number for the quiz" do
+      pending("implement later")
       expect(quiz1.questions.length).to equal(3)
       expect(quiz1.next_number).to equal(3)
 
@@ -52,18 +52,18 @@ describe Quiz do
     end
   end
 
-  describe '.generate_random' do
-    let!(:generated_quiz) { Quiz.generate_random('2', false) }
-    it 'should generate a quiz with 3 random questions (if possible)' do
+  describe ".generate_random" do
+    let!(:generated_quiz) { Quiz.generate_random("2", false) }
+    it "should generate a quiz with 3 random questions (if possible)" do
       [question1, question2, question3].each do |question|
         expect(generated_quiz.questions.to_a).to include(question)
       end
     end
   end
 
-  describe '#get_quest' do
+  describe "#get_quest" do
     let!(:questions_array) { quiz2.get_quest(quiz2.lesson) }
-    it 'should get an array of questions from the same lesson' do
+    it "should get an array of questions from the same lesson" do
       expect(questions_array.length).to equal(3)
       [question1, question2, question3].each do |question|
         expect(questions_array).to include(question)
@@ -71,21 +71,21 @@ describe Quiz do
     end
   end
 
-  describe '#add_numbers' do
-    let!(:generated_quiz) { Quiz.generate_random('2', false) }
+  describe "#add_numbers" do
+    let!(:generated_quiz) { Quiz.generate_random("2", false) }
     let!(:quiz_numbers) { generated_quiz.relationships.map {|q| q.number } }
-    it 'should add numbering to questions' do
+    it "should add numbering to questions" do
       expect(quiz_numbers).to eql([1,2,3])
     end
   end
 
-  describe '#can_add?' do
-    let!(:quiz3) { create :quiz, lesson: '2', retake: true }
+  describe "#can_add?" do
+    let!(:quiz3) { create :quiz, lesson: "2", retake: true }
     before do
       quiz3.questions << question1
       quiz2.questions << question2
     end
-    it 'should detect whether or not a question can be added to a quiz' do
+    it "should detect whether or not a question can be added to a quiz" do
       expect(quiz2.can_add?(question1)).to be false
       expect(quiz3.can_add?(question2)).to be false
 
@@ -94,22 +94,22 @@ describe Quiz do
     end
   end
 
-  describe '.has_quiz' do
-    it 'should return boolean if there is a quiz with given lesson/retake' do
+  describe ".has_quiz" do
+    it "should return boolean if there is a quiz with given lesson/retake" do
       expect(Quiz.has_quiz(quiz1.lesson, quiz1.retake)).to be true
       expect(Quiz.has_quiz(quiz1.lesson, !quiz1.retake)).to be false
     end
   end
 
-  describe '.sort_lesson' do
+  describe ".sort_lesson" do
     before do
       ["0-1", "0-2", "0-3", "10"].each do |lesson|
         Quiz.create(lesson: lesson)
       end
     end
-    let!(:sorted) { ['0-1', '0-2', '0-3', '1', '1', '2', '10'] }
+    let!(:sorted) { ["0-1", "0-2", "0-3", "1", "1", "2", "10"] }
     let!(:lessons) { Quiz.sort_lesson(Quiz.all).map { |q| q.lesson } }
-    it 'should return a sorted array of Quizzes' do
+    it "should return a sorted array of Quizzes" do
       expect(lessons).to eql(sorted)
     end
   end
