f1afef89de21abbfb680a59a94a7b7ff2044c65e
Charles Xue <charlesx2013@gmail.com>
2014-08-10 03:14:02 -0700

    started grading quiz

--- spec/features/grading_quiz_spec.rb
+++ spec/features/grading_quiz_spec.rb
@@ -10,9 +10,75 @@ describe "Grading a Quiz" do
 
   it { should have_content "Quizzes To Grade" }
   it { should have_content "You have no assignments." }
+  it { should have_content "There are no quizzes to grade!"}
 
+  describe "as a staff member" do
+    let!(:student) { create :student }
+    let!(:staff2) { create :staff }
+    let!(:quiz) { create :quiz_with_questions }
+    let!(:taken_quiz) { TakenQuiz.create(quiz_id: quiz.id,
+                                        student_id: student.id,
+                                        staff_id: staff2.id,
+                                        lesson: quiz.lesson,
+                                        retake: quiz.retake) }
+    before do
+      quiz.questions.map do |q|
+        create :submission, question: q, quiz: quiz, student: student
+      end
+      visit grades_path
+      expect(page).to_not have_content "There are no quizzes to grade!"
+      expect(page).to have_content "You have no assignments."
+      expect(page).to have_content "#{taken_quiz}"
+      visit student_quiz_path(taken_quiz.student, taken_quiz.quiz)
+    end
 
-  describe "" do
+    describe "before grading quiz" do
+      it { should have_content "#{quiz}" }
+      it { should have_content "Total: 0.0/10"}
+      it { should_not have_content "Finished Grading!" }
+    end
 
+    describe "grading a question" do
+      before { first(:link, 'Grade Question!').click }
+
+      it { should have_content "Grade Question" }
+
+      describe "should not be valid" do
+
+        it "if the grade is blank" do
+          fill_in "Grade", with: ""
+          click_button "Update Grade!"
+          expect(page).to have_content "can't be blank"
+        end
+
+        it "if comments are blank" do
+          fill_in "Comments", with: ""
+          click_button "Update Grade!"
+          expect(page).to have_content "can't be blank"
+        end
+
+        it "if the grade is not in range of (0..10)" do
+          fill_in "Grade", with: 11
+          click_button "Update Grade!"
+          expect(page).to have_content "Invalid grade"
+        end
+
+        after do
+          expect(page).to_not have_content "#{quiz}"
+          expect(taken_quiz.grade).to eql(0.0)
+        end
+      end
+
+      it "should increase total quiz grade if valid" do
+        fill_in "Grade", with: 5
+        fill_in "Comments", with: "Good Job!"
+        click_button "Update Grade!"
+        expect(page).to have_content "#{quiz}"
+        expect(page).to have_content "Total: 5.0/10"
+        expect(page).to have_content "5.0"
+        expect(page).to have_content "Good Job!"
+        expect(page).to_not have_content "Finished Grading!"
+      end
+    end
   end
 end
