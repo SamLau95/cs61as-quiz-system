610d4837ef0cca979991d096d746c9d7d27c2176
Sam Lau <samlau95@gmail.com>
2014-08-11 06:39:10 -0700

    Fix auth

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,12 +7,9 @@ class Ability
     can [:edit, :update], User, id: user.id
 
     if user.staff?
-      can :manage, :all
-      cannot [:edit, :update], User, :id => (0...user.id).to_a + ((user.id + 1)..User.all.count).to_a
-      cannot :see, Students::DashboardController
+      can :manage, :staff_dashboard
     elsif user.student?
-      can :manage, :self_only
-      # can :show, Staffs::Students::QuizzesController
+      can :manage, :student_dashboard
     end
   end
 end
