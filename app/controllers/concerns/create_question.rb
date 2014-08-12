cf6b0340e506e3979d59f9beab0d206fbd091ce2
Charles Xue <charlesx2013@gmail.com>
2014-08-12 02:06:44 -0700

    service object

---
+++ app/controllers/concerns/create_question.rb
@@ -0,0 +1,12 @@
+# Creates a Question
+class CreateQuestion
+
+  def self.call(question_params)
+    solution = Solution.new question_params.delete :solution_attributes
+    rubric = Rubric.new question_params.delete :rubric_attributes
+    question = Question.new question_params
+    question.solution = solution
+    question.rubric = rubric
+    question
+  end
+end
