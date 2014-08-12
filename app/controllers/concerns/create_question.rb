bbb9d0279d1220d9ea97c1b76e1a6093f71bae71
Charles Xue <charlesx2013@gmail.com>
2014-08-12 03:49:42 -0700

    houng

--- app/controllers/concerns/create_question.rb
+++ app/controllers/concerns/create_question.rb
@@ -1,6 +1,5 @@
 # Creates a Question
 class CreateQuestion
-
   def self.call(question_params)
     solution = Solution.new question_params.delete :solution_attributes
     rubric = Rubric.new question_params.delete :rubric_attributes
