f5e0a051bf4593365dea5494f81fa36fee75f199
Charles Xue <charlesx2013@gmail.com>
2014-08-21 01:03:20 -0700

    moved things to one line

--- app/controllers/staffs/students/quizzes_controller.rb
+++ app/controllers/staffs/students/quizzes_controller.rb
@@ -4,16 +4,11 @@ module Staffs::Students
       @quiz = Quiz.find params[:id]
       @student = Student.find params[:student_id]
       @questions = @quiz.questions
-      @subm = @questions.map do |q|
-        Submission.find_by question_id: q.id, student: @student
-      end
-      @scores = @questions.map do |q|
-        Grade.find_by question_id: q.id, student: @student
-      end
+      @subm = @questions.map { |q| q.submissions.find_by student: @student }
+      @scores = @questions.map { |q| q.grades.find_by student: @student }
       @ques_subm = QuizSubmission.new(@questions, @subm, @scores).ques_subm
       @grade = TakenQuiz.find_by quiz: @quiz, student: @student
       @request = Regrade.find_by quiz: @quiz, student: @student
-      @regrade = Regrade.new
       @not_graded = @scores.inject(false) { |q, q2| q || q2.nil? }
     end
 
