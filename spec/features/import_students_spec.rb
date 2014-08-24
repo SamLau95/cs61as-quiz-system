65b1b360dadcd6fdeb3e878eff85f7ff49dad2ac
Charles Xue <charlesx2013@gmail.com>
2014-08-24 02:27:02 -0700

    added tests for importing students

--- spec/features/import_students_spec.rb
+++ spec/features/import_students_spec.rb
@@ -10,4 +10,24 @@ describe 'Import students' do
   end
 
   it { should have_content 'Import Students' }
+
+  describe "by filling in the import box with 2 students" do
+    before do
+      Student.destroy_all
+      fill_in "import", with: "cs61as-aa \n cs61as-ab"
+    end
+
+    it "should increase the student count by 2" do
+      expect { click_button "Create students!" }.to change(Student, :count).by 2
+    end
+
+    it "should have initial passwords" do
+      click_button "Create students!"
+      expect(
+        Student.all.inject do |s1, s2|
+          !s1.first_password.blank? && !s2.first_password.blank?
+        end
+      ).to eql true
+    end
+  end
 end
