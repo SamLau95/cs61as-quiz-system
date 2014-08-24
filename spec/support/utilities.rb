6101beed663b4e48f1b59dfc8f886fa11095a08c
Charles Xue <charlesx2013@gmail.com>
2014-08-24 03:07:54 -0700

    moved things around

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,19 @@ def sign_out
   visit root_path
   click_link 'Sign Out'
 end
+
+def fill_in_attributes
+  fill_in 'First name',        with: new_first
+  fill_in 'Last name',         with: new_last
+  fill_in 'Email',        with: new_email
+  fill_in 'New Password', with: new_pass
+  fill_in 'Confirm',      with: new_pass
+end
+
+def expect_attributes_to_be_updated(user)
+  user.reload
+  expect(user.first_name).to eq new_first
+  expect(user.last_name).to eq new_last
+  expect(user.email).to eq new_email
+  expect(user.valid_password? new_pass).to be_true
+end
